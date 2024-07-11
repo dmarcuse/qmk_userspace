e41803f778eaaab867f7473f143a029a2c23078e
Dana Marcuse <dana@marcuse.us>
2024-07-11 14:01:39 -0400

    Fix encoder direction

--- keyboards/keebio/sinc/rev4/keymaps/dmarcuse/keymap.c
+++ keyboards/keebio/sinc/rev4/keymaps/dmarcuse/keymap.c
@@ -23,18 +23,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
+    if (clockwise) {
+        tap_code(KC_VOLD);
+    } else {
+        tap_code(KC_VOLU);
     }
     return false;
 }
